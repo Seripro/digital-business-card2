b899f16d7a17a1078732fc5a21c9e0d0fc05980c
keigo <2364138keigo@gmail.com>
2026-06-21 22:08:43 +0900

    routerの設定を追加

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine
 from app.models import skill, user_skill, user
 from app.routers.skill import router as skill_router
+from app.routers.user_skill import router as user_skill_router
+from app.routers.user import router as user_router
 
 # サーバー起動時にテーブルを作成
 skill.Base.metadata.create_all(bind=engine)
@@ -14,6 +16,8 @@ user.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 security = HTTPBearer(auto_error=False)
 app.include_router(skill_router)
+app.include_router(user_skill_router)
+app.include_router(user_router)
 
 app.add_middleware(
     CORSMiddleware,
