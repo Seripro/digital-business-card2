7ee0c39d1fdaf95e94f0f53a7bc7eafbaec39e4f
keigo <2364138keigo@gmail.com>
2026-06-21 20:56:16 +0900

    skillのservice層を作成

---
+++ backend/app/services/skill.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import Session
+
+from app.schemas.skill import SkillSearchRequest
+from app.repositories.skill import SkillRepository
+
+class SkillService:
+    @staticmethod
+    def get_all_skills(data: SkillSearchRequest, db: Session):
+        skills = SkillRepository.get_all_skills(data, db)
+        return skills
